e225753ddd4e9717df075b9e96825b3829ccd13f
Arve Knudsen <arve.knudsen@gmail.com>
2015-01-01 17:55:21 +0100

    Update for Meteor 1.x

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
-  summary: "Easily include ace, receive reactive varibles for cursor position, editor contents, etc"
+  summary: "Easily include ace, receive reactive varibles for cursor position, editor contents, etc",
+  version: '1.0.0',
+  name: 'aknudsen:reactive-ace',
+  git: 'https://github.com/aknuds1/meteor-reactive-ace.git',
 });
-
 
 var bundlerApi = null
 var path = Npm.require("path");
@@ -9,11 +11,13 @@ var fs = Npm.require("fs");
 var packagePath = path.join(path.resolve("."), "packages", "reactive-ace");
 
 Package.on_use(function (api, where) {
-  api.use(['jquery', "templating", "coffeescript", "underscore", "deps"], ["client"]);
+  api.versionsFrom('1.0.2')
+
+  api.use(['jquery', "templating", "coffeescript", "underscore", "deps"], "client");
 
   var files = fs.readdirSync(path.join(packagePath, "ace-builds", "src"));
   files.forEach(function(file){
-    if (file === "snippets"){return;}
+    if (file === "snippets") { return; }
     api.add_files(path.join("ace-builds", "src", file), "client", {isAsset: true});
   });
 
@@ -22,7 +26,6 @@ Package.on_use(function (api, where) {
     snippetPath = path.join("ace-builds", "src", "snippets", file)
     api.add_files(snippetPath, "client", {isAsset: true});
   })
-
 
   api.add_files(["ace-builds/src/ace.js", "ace-builds/src/ext-modelist.js", "lib/utils.coffee", "lib/crc32.js", "lib/esprima.js", "editor.coffee", "editorSetup.coffee", "hack.hack", "templates.html"], "client");
 });
